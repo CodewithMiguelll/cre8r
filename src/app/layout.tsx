a7d02030213d33ae11997b30dd7b435b048f0cef
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2026-03-05 17:56:33 +0100

    feat: add Playfair and Sora fonts with configuration in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,23 @@
 import type { Metadata } from "next";
+import { Playfair_Display,Sora } from "next/font/google";
+
+/*
+------------------FONT CONFIGURATION------------------
+Headings: Playfair Display Bold
+Subheadings: Playfair Display Medium
+Body: Sora
+*/
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+});
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+});
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,7 +34,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       suppressHydrationWarning
-        className={`antialiased`}
+        className={`${playfair.variable} ${sora.variable} antialiased`}
       >
         {children}
       </body>
