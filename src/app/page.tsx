d54d5ede4afc99b7e881af8c2cc9d654de2bf4ec
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2026-03-25 07:01:30 +0100

    feat: restructure Home and Navigation components for improved layout and functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-playfair font-medium">Welcome to Cre8r!</h1>
-      <h2 className="text-lg mt-4 font-playfair">
-        The home for Nigerian creatives.
-      </h2>
-      <p className="font-sora font-medium">
-        Discover and connect with talented Nigerian artists, designers, writers, and creators.
-      </p>
+      <div className="text-center mt-5">
+        <h1 className="text-3xl font-playfair font-medium">
+          Welcome to Cre8r!
+        </h1>
+        <h2 className="text-lg mt-4 font-playfair">
+          The home for Nigerian creatives.
+        </h2>
+        <p className="font-sora font-medium">
+          Discover and connect with talented Nigerian artists, designers,
+          writers, and creators.
+        </p>
+      </div>
     </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import Link  from "next/link";
+import { useState } from "react";
+import { X, Menu } from "lucide-react";
+
 const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const links = [
     { name: "Explore", href: "/" },
     { name: "About", href: "/about" },
+    { name: "Exhibits", href: "/exhibits" },
     { name: "Profile", href: "/profile" },
   ];
   const niches = [
@@ -13,18 +21,31 @@ const Navigation = () => {
   ];
 
   return (
-    <nav className="p-5 max-w-xl mx-auto py-4 flex items-center justify-between">
-      <div className="nav-links">
-        {links.map((link) => (
-          <a key={link.href} href={link.href}>
-            {link.name}
+    <nav className="w-full py-6 px-5 flex items-center justify-between">
+      {/* Header Logo */}
+      <Link href="/">
+        <h1 className="text-2xl font-playfair uppercase font-bold">Cre8r</h1>
+      </Link>
+      <div className="hidden md:flex gap-8 items-center">
+        {niches.map((niche) => (
+          <a
+            key={niche.href}
+            href={niche.href}
+            className="hover:opacity-70 transition-opacity font-sora"
+          >
+            {niche.name}
           </a>
         ))}
       </div>
-      <div className="flex gap-3 ">
-        {niches.map((niche) => (
-          <a key={niche.href} href={niche.href}>
-            {niche.name}
+
+      <div className="hidden md:flex gap-8 items-center">
+        {links.map((link) => (
+          <a
+            key={link.href}
+            href={link.href}
+            className="hover:opacity-70 transition-opacity font-sora"
+          >
+            {link.name}
           </a>
         ))}
       </div>
